cbe512b784c37c5e30881a4b64f8df3a4679ec2a
amrebada <amr.app.engine@gmail.com>
2020-01-04 04:38:53 +0400

    task 3 integration

--- src/components/Questions/tasks/index.js
+++ src/components/Questions/tasks/index.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Task1 from "./task1";
 import Task2 from "./task2";
+import Task3 from "./task3";
 
 const Tasks = props => {
   return (
     <>
       <Task1 />
       <Task2 />
+      <Task3 />
     </>
   );
 };
---
+++ src/components/Questions/tasks/task3.js
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import classes from "./tasks.module.css";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import authContext from "../../../states/store";
+import { BACKEND_URL } from "../../../constants";
+const TASK3Query = gql`
+  {
+    MostAppearedSpecies {
+      name
+      value {
+        total
+        people
+      }
+    }
+  }
+`;
+
+const Task3 = props => {
+  const { loading, error, data } = useQuery(TASK3Query);
+  const [state, dispatch] = useContext(authContext);
+  const refreshToken = () => {
+    fetch(`${BACKEND_URL}/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        refreshToken: state.refreshToken
+      })
+    })
+      .then(response => response.json())
+      .then(value => {
+        if (value.error) {
+          dispatch({ type: "LOGOUT" });
+        } else {
+          dispatch({ type: "UPDATE", accessToken: value.accessToken });
+        }
+      })
+      .catch(err => {
+        dispatch({ type: "LOGOUT" });
+      });
+  };
+  return (
+    <div className={classes.container}>
+      <p className={classes.question}>
+        What character (person) appeared in most of the Star Wars films?
+      </p>
+      {loading && <p className={classes.answer}>Loading ...</p>}
+      {error && <p className={classes.bad}>error ...{refreshToken()}</p>}
+      {data &&
+        data.MostAppearedSpecies.map(sp => (
+          <p className={classes.answer}>
+            {sp.name} ({sp.value.total})
+          </p>
+        )).slice(0, 5)}
+    </div>
+  );
+};
+
+export default Task3;
