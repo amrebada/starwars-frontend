12fd9246ccc028dc3b2edf381605cf49c3159ad4
amrebada <amr.app.engine@gmail.com>
2020-01-04 04:47:20 +0400

    task 4 integration

--- src/components/Questions/tasks/index.js
+++ src/components/Questions/tasks/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Task1 from "./task1";
 import Task2 from "./task2";
 import Task3 from "./task3";
+import Task4 from "./task4";
 
 const Tasks = props => {
   return (
@@ -10,6 +11,7 @@ const Tasks = props => {
       <Task1 />
       <Task2 />
       <Task3 />
+      <Task4 />
     </>
   );
 };
---
+++ src/components/Questions/tasks/task4.js
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import classes from "./tasks.module.css";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import authContext from "../../../states/store";
+import { BACKEND_URL } from "../../../constants";
+const TASK4Query = gql`
+  {
+    MostPlanetHasPilots {
+      name
+      value {
+        total
+        people {
+          name
+          species
+        }
+      }
+    }
+  }
+`;
+
+const Task4 = props => {
+  const { loading, error, data } = useQuery(TASK4Query);
+  const [state, dispatch] = useContext(authContext);
+  const refreshToken = () => {
+    fetch(`${BACKEND_URL}/token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        refreshToken: state.refreshToken
+      })
+    })
+      .then(response => response.json())
+      .then(value => {
+        if (value.error) {
+          dispatch({ type: "LOGOUT" });
+        } else {
+          dispatch({ type: "UPDATE", accessToken: value.accessToken });
+        }
+      })
+      .catch(err => {
+        dispatch({ type: "LOGOUT" });
+      });
+  };
+  return (
+    <div className={classes.container}>
+      <p className={classes.question}>
+        What character (person) appeared in most of the Star Wars films?
+      </p>
+      {loading && <p className={classes.answer}>Loading ...</p>}
+      {error && <p className={classes.bad}>error ...{refreshToken()}</p>}
+      {data &&
+        data.MostPlanetHasPilots.map(planet => (
+          <p className={classes.answer}>
+            Planet: {planet.name} - Pilots: ({planet.value.total}){" "}
+            {planet.value.people
+              .map(p => `${p.name} - ${p.species || "unknown"}`)
+              .join(", ")}
+          </p>
+        )).slice(0, 5)}
+    </div>
+  );
+};
+
+export default Task4;
